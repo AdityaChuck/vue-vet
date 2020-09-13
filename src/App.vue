071e1175a4545e5200fe210e8744492c8df8e499
AdityaChuck <adityanarayanmahapatra00@gmail.com>
2020-09-14 00:51:15 +0530

    first vue app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,100 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main-app" className="container">
+    <div class="row justify-content-center">
+      <AddAppointment @add="addItem" />
+      <SearchAppointment @searchRecords="searchAppointments" 
+        :filterKey="filterKey" :filterDir="filterDir" 
+        @requestkey="changeKey" @requestdir="changeDir"
+      />
+      <AppointmentList
+        :appointments="filteredApts"
+        @remove="removeItem"
+        @edit="editItem"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddAppointment from "./components/AddAppointment";
+import AppointmentList from "./components/AppointmentList";
+import SearchAppointment from "./components/SearchAppointment";
+
+import axios from "axios";
+import _ from "lodash";
 
 export default {
-  name: 'App',
+  name: "MainApp",
+  data: () => ({
+    appointments: [],
+    aptIndex: 0,
+    searchTerms: "",
+    filterKey: "petName",
+    filterDir: "asc",
+  }),
   components: {
-    HelloWorld
-  }
-}
+    AppointmentList,
+    AddAppointment,
+    SearchAppointment,
+  },
+  computed: {
+    searchApts: function() {
+      return this.appointments.filter(
+        (item) =>
+          String(item.petName)
+            .toLowerCase()
+            .match(String(this.searchTerms).toLowerCase()) ||
+          String(item.petOwner)
+            .toLowerCase()
+            .match(String(this.searchTerms).toLowerCase()) ||
+          String(item.aptNotes)
+            .toLowerCase()
+            .match(String(this.searchTerms).toLowerCase())
+      );
+    },
+    filteredApts: function() {
+      return _.orderBy(
+        this.searchApts,
+        (item) => {
+          return item[this.filterKey].toLowerCase()
+        },
+        this.filterDir
+      )
+    },
+  },
+  methods: {
+    removeItem: function(apt) {
+      this.appointments = _.without(this.appointments, apt);
+    },
+    editItem: function(id, field, text) {
+      const aptIndex = _.findIndex(this.appointments, {
+        aptId: id,
+      });
+      this.appointments[aptIndex][field] = text;
+    },
+    addItem: function(apt) {
+      apt.aptId = this.aptIndex;
+      this.aptIndex++;
+      this.appointments = [apt, ...this.appointments];
+    },
+    searchAppointments: function(terms) {
+      this.searchTerms = terms;
+    },
+    changeKey: function(value){
+      this.filterKey = value
+    },
+    changeDir: function(value){
+      this.filterDir = value
+    }
+  },
+  mounted() {
+    axios.get("./data/appointments.json").then((response) => {
+      this.appointments = response.data.map((item) => {
+        item.aptId = this.aptIndex;
+        this.aptIndex++;
+        return item;
+      });
+    });
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchAppointment.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="col-12 col-md-10 col-lg-7">
+    <div class="input-group my-3">
+      <input
+        id="SearchApts"
+        placeholder="Search"
+        type="text"
+        class="form-control"
+        aria-label="Search Appointments"
+        v-model="searchTerm"
+      />
+
+      <div class="input-group-append">
+        <button
+          type="button"
+          class="btn btn-primary dropdown-toggle"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          Sort by
+          <span class="caret"></span>
+        </button>
+
+        <div class="dropdown-menu dropdown-menu-right">
+          <a
+            href="#"
+            class="dropdown-item d-flex justify-content-between"
+            id="petName"
+            @click="$emit('requestkey', 'petName')"
+          >
+            Pet Name
+            <font-awesome-icon icon="check" v-if="filterKey === 'petName'"/>
+          </a>
+
+          <a
+            class="dropdown-item d-flex justify-content-between"
+            href="#"
+            id="aptDate"
+            @click="$emit('requestkey', 'aptDate')"
+          >
+            Date
+            <font-awesome-icon icon="check" v-if="filterKey === 'aptDate'" />
+          </a>
+
+          <a
+            href="#"
+            class="dropdown-item d-flex justify-content-between"
+            id="ownerName"
+            @click="$emit('requestkey', 'petOwner')"
+          >
+            Owner
+            <font-awesome-icon icon="check" v-if="filterKey === 'petOwner'" />
+          </a>
+
+          <div class="dropdown-divider" role="separator"></div>
+
+          <a
+            class="dropdown-item d-flex justify-content-between"
+            href="#"
+            id="asc"
+            @click="$emit('requestdir', 'asc')"
+          >
+            Asc
+            <font-awesome-icon icon="check" v-if="filterDir === 'asc'"  />
+          </a>
+
+          <a
+            class="dropdown-item d-flex justify-content-between"
+            href="#"
+            id="desc"
+            @click="$emit('requestdir', 'desc')"
+          >
+            Desc
+            <font-awesome-icon icon="check" v-if="filterDir === 'desc'" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+export default {
+    name:"SearchAppointment",
+    components:{
+        FontAwesomeIcon
+    },
+    data: () => ({
+      searchTerm:"",
+    }),
+    props:["filterKey", "filterDir"],
+    watch: {
+      searchTerm: function(){
+        this.$emit("searchRecords", this.searchTerm)
+      }
+    }
+}
+</script>
